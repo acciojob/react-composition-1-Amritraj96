6e9ff98093548183902be6f2fc45383ea789bf4b
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-01-13 21:31:48 +0530

    Update Tab.js

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,22 +1,23 @@
-export default function Tab(){
-  funtion dis1(){
-    <p>This is the content for Tab 1</p>
+import { useState } from "react";
+
+export default function Tab() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  function renderContent() {
+    if (activeTab === 1) return <p>This is the content for Tab 1</p>;
+    if (activeTab === 2) return <p>This is the content for Tab 2</p>;
+    if (activeTab === 3) return <p>This is the content for Tab 3</p>;
   }
-  funtion dis2(){
-    <p>This is the content for Tab 1</p>
-  }
-  funtion dis3(){
-    <p>This is the content for Tab 1</p>
-  }
+
   return (
     <>
       <ul>
-        <li onClick={dis1}>Tab 1</li>
-        <li onClick={dis2}>Tab 2</li>
-        <li onClick={dis3}>Tab 3</li>
-        
+        <li onClick={() => setActiveTab(1)}>Tab 1</li>
+        <li onClick={() => setActiveTab(2)}>Tab 2</li>
+        <li onClick={() => setActiveTab(3)}>Tab 3</li>
       </ul>
-      
+
+      {renderContent()}
     </>
-  )
+  );
 }
